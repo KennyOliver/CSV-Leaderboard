2792c9d1c9751395cf8a692109b8a1455c34763e
Kenny <kenny_oliver@icloud.com>
2021-03-04 15:15:16 +0000

    [+] Print file name before printing records of that file [+] Changed searchLeaderboard() to scrape_leaderboard() [+] Using range to check, rather than checking if input in array [+] Print available files using for and range, rather than lots of \n and \t [+] Added header line to each CSV, but skipping it when printing records [+] Minor UI tweaks

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import csv #dealing with csv files here
 #====================
-def fileLeaderboards():
+def scrape_leaderboard():
   print("Choose a file")
-  print("\t[1] day1.csv\n\t[2] day2.csv\n\t[3] day3.csv")
+  for i in range(1,3+1):
+    print(f"\t[{i}] day{i}.csv")
   fileNum = int(input("\t--> "))
-  while fileNum not in [1,2,3]:
+  while fileNum not in range(1,3+1):
     fileNum = int(input("\t--> "))
   
   file = open(rf"database/day{fileNum}.csv",'r')
@@ -14,8 +15,10 @@ def fileLeaderboards():
   #saveName
   
   print("-" * 30)
-  print("NAME\tGAME\t\tSCORE")
   
+  print(f"<-- day{fileNum}.csv -->")
+  print("NAME\tGAME\t\tSCORE")
+  next(reader,None) #skips header line
   for rec in reader:
     name,game,score = rec[0],rec[1],rec[2]
     print(name,game,score)
@@ -25,7 +28,7 @@ def fileLeaderboards():
   
   print("-" * 30)
   
-  print("Best Player: %s\nGame: %s\nScore: %s" % (saveRec[0], saveRec[1], saveRec[2]))
+  print("Best Player: %s\nGame: %s\nHighscore: %s" % (saveRec[0], saveRec[1], saveRec[2]))
   file.close()
 #====================
 # MAIN PROGRAM
@@ -33,8 +36,8 @@ print("<-- CSV Game Leaderboard -->")
 print("-" * 30)
 run = 'Y'
 while run in ['Y','YES']:
-  fileLeaderboards()
+  scrape_leaderboard()
   print("-" * 30)
-  run = input("Analyse another file?\n[Y] [N]\n--> ").upper()
+  run = input("Scrape another file?\n\t[Y] [N]\n\t--> ").upper()
   print("-" * 30)
 print("\n• That's it! •")
