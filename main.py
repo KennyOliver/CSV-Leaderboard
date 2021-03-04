419f173b8e666e0cdf55d50507e79a921ad01d04
Kenny <kenny_oliver@icloud.com>
2021-03-04 12:04:11 +0000

    [+] Using raw fstrings for file selection adding dividers “-” [+] Spaced out code for readability [+] Improved UI [+] Program title

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
-import csv #Dealing with csv files here
+import csv #dealing with csv files here
+#====================
 def fileLeaderboards():
-  DAY1, DAY2, DAY3 = 'database/day1.csv', 'database/day2.csv', 'database/day3.csv' #List of files
-  selectFile = int(input("Select file number\n[1-3]\n--> ")) #Select file
-  while selectFile not in [1,2,3]:
-    selectFile = int(input("Select file number\n[1-3]\n--> "))
-  if selectFile == 1:
-    chosenFile = DAY1
-  elif selectFile == 2:
-    chosenFile = DAY2
-  elif selectFile == 3:
-    chosenFile = DAY3
-  else:
-    None
-  file = open(chosenFile,'r') #Open the chosen file
+  print("Choose a file")
+  print("\t[1] day1.csv\n\t[2] day2.csv\n\t[3] day3.csv")
+  fileNum = int(input("\t--> "))
+  while fileNum not in [1,2,3]:
+    fileNum = int(input("\t--> "))
+  
+  file = open(rf"database/day{fileNum}.csv",'r')
   reader = csv.reader(file)
-  highscore = 0 #To be compared to later
-  saveName = ''
-  print("NAME\tGAME\tSCORE")
-  for rec in reader: #rec is a record
-    name, game, score = rec[0], rec[1], rec[2] #Having commas split the record means we do not have to worry about lengths, unlike with a single record where we need to know which parts to index
-    print(name, game, score)
-    if int(score) > highscore: #Every score encountered, that is higher, is saved to a variable
+  
+  highscore = 0 #to be compared to later
+  #saveName
+  
+  print("-" * 30)
+  print("NAME\tGAME\t\tSCORE")
+  
+  for rec in reader:
+    name,game,score = rec[0],rec[1],rec[2]
+    print(name,game,score)
+    if int(score) > highscore: #every score encountered, if higher, is saved to a variable
       highscore = int(score)
-      saveRecord = rec
-  print("\n")
-  print("Highest Player: %s\nGame: %s\nScore: %s" % (saveRecord[0], saveRecord[1], saveRecord[2]))
-  #chosenFile.close() -- Doesn't work because chosenFile is a string!
-  close() #Closes the program
-
-
+      saveRec = rec
+  
+  print("-" * 30)
+  
+  print("Best Player: %s\nGame: %s\nScore: %s" % (saveRec[0], saveRec[1], saveRec[2]))
+  file.close()
+#====================
 # MAIN PROGRAM
-fileLeaderboards()
-Y_or_N = input("Analyse another file?\n[Y] [N]\n--> ") #Find whether user wants to see more
-while Y_or_N == 'Y':
+print("<-- CSV Game Leaderboard -->")
+print("-" * 30)
+run = 'Y'
+while run in ['Y','YES']:
   fileLeaderboards()
+  print("-" * 30)
+  run = input("Analyse another file?\n[Y] [N]\n--> ").upper()
+  print("-" * 30)
 print("\n• That's it! •")
